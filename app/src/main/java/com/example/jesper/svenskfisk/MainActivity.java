5606f05616db5ea57cd948a41ca605b6104ecc3e
Jesper Bergström <jesper.b96@gmail.com>
2017-04-18 00:00:35 +0200

    Start-screen with 3 buttons

---
+++ app/src/main/java/com/example/jesper/svenskfisk/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.jesper.svenskfisk;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button searchBtn;
+    public Button idBtn;
+    public Button browseBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        onSearchBtnTap();
+        onBrowseBtnTap();
+        onIdBtnTap();
+    }
+
+    public void onSearchBtnTap() {
+        searchBtn = (Button) findViewById(R.id.searchBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent search = new Intent(MainActivity.this, SearchActivity.class);
+
+                startActivity(search);
+            }
+        });
+    }
+
+    public void onBrowseBtnTap() {
+        browseBtn = (Button) findViewById(R.id.browseBtn);
+        browseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browse = new Intent(MainActivity.this, BrowseActivity.class);
+
+                startActivity(browse);
+            }
+        });
+    }
+
+    public void onIdBtnTap() {
+        idBtn = (Button) findViewById(R.id.idBtn);
+        idBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent identify = new Intent(MainActivity.this, IdActivity.class);
+
+                startActivity(identify);
+            }
+        });
+    }
+}
